2982af91dcf42d376e95c8a2b150f0f66b9bb733
Netik59 <modznetik@gmail.com>
2024-04-10 17:01:04 +0200

    Component Account

--- argentbank-app/src/app/pages/User.js
+++ argentbank-app/src/app/pages/User.js
@@ -2,6 +2,7 @@ import { useNavigate } from "react-router-dom";
 import { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from "react-redux";
 import { updateUserNameAsync, getUserProfileAsync } from "../../features/authSlice";
+import { Account } from "../../common/components/Account";
 
 export const User = () => {
 
@@ -98,36 +99,26 @@ export const User = () => {
                     )}
                 </div>
                 <h2 className="sr-only">Accounts</h2>
-                <section className="account">
-                    <div className="account-content-wrapper">
-                        <h3 className="account-title">Argent Bank Checking (x8349)</h3>
-                        <p className="account-amount">$2,082.79</p>
-                        <p className="account-amount-description">Available Balance</p>
-                    </div>
-                    <div className="account-content-wrapper cta">
-                        <button className="transaction-button">View transactions</button>
-                    </div>
-                </section>
-                <section className="account">
-                    <div className="account-content-wrapper">
-                        <h3 className="account-title">Argent Bank Savings (x6712)</h3>
-                        <p className="account-amount">$10,928.42</p>
-                        <p className="account-amount-description">Available Balance</p>
-                    </div>
-                    <div className="account-content-wrapper cta">
-                        <button className="transaction-button">View transactions</button>
-                    </div>
-                </section>
-                <section className="account">
-                    <div className="account-content-wrapper">
-                        <h3 className="account-title">Argent Bank Credit Card (x8349)</h3>
-                        <p className="account-amount">$184.30</p>
-                        <p className="account-amount-description">Current Balance</p>
-                    </div>
-                    <div className="account-content-wrapper cta">
-                        <button className="transaction-button">View transactions</button>
-                    </div>
-                </section>
+                <div>
+                    <Account
+                        title="Argent Bank Checking"
+                        accountNumber="x8349"
+                        amount="$2,082.79"
+                        description="Available Balance"
+                    />
+                    <Account
+                        title="Argent Bank Savings"
+                        accountNumber="x6712"
+                        amount="$10,928.42"
+                        description="Available Balance"
+                    />
+                    <Account
+                        title="Argent Bank Credit Card"
+                        accountNumber="x8349"
+                        amount="$184.30"
+                        description="Current Balance"
+                    />
+                </div>
             </div>
         </main>
     )
